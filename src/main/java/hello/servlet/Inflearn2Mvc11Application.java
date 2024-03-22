1898a30cb4c1790653aca51a2137b47508303d1f
yangyechan <yac392045@mokpo.ac.kr>
2024-03-22 22:39:49 +0900

    "first commit"

---
+++ src/main/java/hello/servlet/Inflearn2Mvc11Application.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Inflearn2Mvc11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Inflearn2Mvc11Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/servlet/Inflearn2Mvc11ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Inflearn2Mvc11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
